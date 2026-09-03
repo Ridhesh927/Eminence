80bd382a4b4e26d81911538633640dacad08419b
Ridhesh927 <ridheshmahajan@gmail.com>
2026-09-03 10:40:23 +0530

    changes

--- backend/src/routes/walletRoutes.js
+++ backend/src/routes/walletRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getWallet, applyReferralCode } = require('../controllers/walletController');
-const { protect } = require('../middleware/auth');
+const protect = require('../middleware/authMiddleware');
 
 router.get('/', protect, getWallet);
 router.post('/referral', protect, applyReferralCode);
